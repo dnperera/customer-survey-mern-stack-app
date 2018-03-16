71106f0c80cfbcc8920499fd5b02a79b66dcf38a
Dayan Perera <dayannp@gmail.com>
2018-03-16 12:56:15 -0700

    Add Passport Authentication with Google

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const keys = require('./config/keys');
 const app = express();
-//Google Client ID  207912861586-njdbp0qr74gkkg2guiejjo1nmf8nvgee.apps.googleusercontent.com
-//Google Client Secret RlzJnKTMb3jiyHCOCiT2a6uA
-//passport.use(new GoogleStrategy());
 
-app.get('/', (req, res) => {
-	res.send({ project: 'Customer feedback survey MERN Stack App..' });
-});
+//configure passport to use Google Oauth
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback',
+		},
+		accessToken => {
+			console.log(accessToken);
+		}
+	)
+);
+
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: ['profile', 'email'],
+	})
+);
+
+//route to handle google callback url
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`Server started listening on port ${PORT}`);
