c781623962de474597a2b35954c348ec5891891f
Dayan Perera <dayannp@gmail.com>
2018-03-16 09:14:26 -0700

    initial commit for first Heroku deployment !

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-const PORT = process.env.PORT || 5000;
+const app = express();
+//Google Client ID  207912861586-njdbp0qr74gkkg2guiejjo1nmf8nvgee.apps.googleusercontent.com
+//Google Client Secret RlzJnKTMb3jiyHCOCiT2a6uA
+//passport.use(new GoogleStrategy());
 
 app.get('/', (req, res) => {
-	res.send({ hi: 'This is survey app ....' });
+	res.send({ project: 'Customer feedback survey MERN Stack App..' });
 });
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`Server started listening on port ${PORT}`);
 });
