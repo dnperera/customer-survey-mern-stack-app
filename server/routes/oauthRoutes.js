f65fc59e14e24d5e4f67f8bebb549d00412e5dfc
Dayan Perera <dayannp@gmail.com>
2018-03-21 22:58:14 -0700

    complete google user authentication

--- server/routes/oauthRoutes.js
+++ server/routes/oauthRoutes.js
@@ -11,7 +11,13 @@ module.exports = app => {
 	//route to handle google callback url
 	app.get('/auth/google/callback', passport.authenticate('google'));
 
+	app.get('/api/logout', (req, res) => {
+		req.logout();
+		res.send('req.user');
+	});
+
 	app.get('/api/current_user', (req, res) => {
+		console.log(req.user);
 		res.send(req.user);
 	});
 };
--- server/services/passport.js
+++ server/services/passport.js
@@ -3,7 +3,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
-const User = mongoose.model('users');
+const User = mongoose.model('users'); // fetch User model or schema from mongoose
 
 passport.serializeUser((user, done) => {
 	done(null, user.id);
